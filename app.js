46829e06116f3d8a1ef12bc80d3523ad4127acb3
oratake <smith.shimomura@gmail.com>
2023-01-10 01:10:21 +0900

    feat: botに挨拶するとメンション返してくれるようにした

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const { App } = require('@slack/bolt');
 // ボットトークンと Signing Secret を使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
+
+app.message('hellobot', async ({ message, say }) => {
+  await say(`Hi, <@${message.user}>.`);
 });
 
 (async () => {
